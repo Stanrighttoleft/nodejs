abbbb2fdc2fe6f1169cb5444f51da9e0f5241cee
stan <moon_saga@yahoo.com.tw>
2025-03-15 14:20:35 +0800

    create routes and also download extension thunder client to test out the routers function

--- 05routes/server.js
+++ 05routes/server.js
@@ -41,9 +41,7 @@ app.use('/subdir',express.static(path.join(__dirname,'/public')));
 //routes
 app.use('/',require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
-
-
-
+app.use('/employees',require('./routes/api/employees'));
 
 //the following 404 page statement can also show like below
 /*
