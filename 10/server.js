0c1fa8ef147d9243873172e4aaba22b2089923d2
stan <moon_saga@yahoo.com.tw>
2025-03-17 20:32:12 +0800

    set up everything to conncet to the server

--- 10/server.js
+++ 10/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express= require('express');
 const app=express();
 const path=require('path');
@@ -8,8 +9,13 @@ const errorHandler=require('./middleware/errorHandler');
 const verifyJWT=require('./middleware/verifyJWT')
 const cookieParser=require('cookie-parser')
 const credentials=require('./middleware/credentials')
+const mongoose=require('mongoose');
+const connectDB=require('./config/dbConn');
 
 const PORT=process.env.PORT || 3500;
+
+//connect to MongoDB
+connectDB();
 
 //custom middleware.logger
 app.use(logger);
@@ -71,12 +77,12 @@ app.all('*',(req,res)=>{
 
 app.use(errorHandler);
 
+//the app only listen to request when connect to the server
+mongoose.connection.once('open',()=>{
+    console.log('Connected to MongoDB');
+    app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
+})
 
-
-
-
-
-app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
 
 
 
